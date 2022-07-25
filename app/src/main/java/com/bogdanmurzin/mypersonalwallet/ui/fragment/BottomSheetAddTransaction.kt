b9f7949bb7611788e8a034b531321a467a1afae6
Bogdan Murzin <murzinbt@gmail.com>
2022-07-25 18:56:14 +0300

    feat: implement bottomSheet

--- app/src/main/java/com/bogdanmurzin/mypersonalwallet/ui/fragment/BottomSheetAddTransaction.kt
+++ app/src/main/java/com/bogdanmurzin/mypersonalwallet/ui/fragment/BottomSheetAddTransaction.kt
@@ -1,11 +1,16 @@
 package com.bogdanmurzin.mypersonalwallet.ui.fragment
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import com.bogdanmurzin.mypersonalwallet.databinding.FragmentBottomsheetAddTransactionBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.text.NumberFormat
 
 
 class BottomSheetAddTransaction : BottomSheetDialogFragment() {
@@ -18,7 +23,29 @@ class BottomSheetAddTransaction : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentBottomsheetAddTransactionBinding.inflate(layoutInflater)
+        setupEditText()
         return binding.root
+    }
+
+    private fun setupEditText() {
+        val textview = binding.transactionAmountTv
+        textview.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    textview.removeTextChangedListener(this)
+                    val cleanString: String = s.replace("""[$,.]""".toRegex(), "")
+                    val parsed = cleanString.toDouble()
+                    val formatted = NumberFormat.getCurrencyInstance().format((parsed / 100))
+                    textview.setText(formatted)
+                    textview.setSelection(formatted.length)
+                    textview.addTextChangedListener(this)
+                }
+            }
+        })
     }
 
     companion object {
