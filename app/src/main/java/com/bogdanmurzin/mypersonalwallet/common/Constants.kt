ae64149f1a2a50ff7fbf59d04fc2f21397996eb4
murzinbt@gmail.com <murzinbt@gmail.com>
2022-08-10 15:51:26 +0300

    fix: some layout

--- app/src/main/java/com/bogdanmurzin/mypersonalwallet/common/Constants.kt
+++ app/src/main/java/com/bogdanmurzin/mypersonalwallet/common/Constants.kt
@@ -9,5 +9,5 @@ object Constants {
     val DOLLAR_OR_COMA_REGEX = Regex("[$,]")
     val DOLLAR_OR_COMA_OR_DOT_REGEX = Regex("[$,.]")
     const val RETROFIT_ICONS_NAMED = "RetrofitIconsClient"
-    const val SPAN_COUNT = 6
+    const val SPAN_COUNT = 4
 }
