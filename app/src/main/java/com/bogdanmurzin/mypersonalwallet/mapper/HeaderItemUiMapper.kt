705f337e5a22618b507507601d7a0b51dddfc7c5
murzinbt@gmail.com <murzinbt@gmail.com>
2022-08-12 15:05:17 +0300

    refactor: HeaderItemUiMapper

--- app/src/main/java/com/bogdanmurzin/mypersonalwallet/mapper/HeaderItemUiMapper.kt
+++ app/src/main/java/com/bogdanmurzin/mypersonalwallet/mapper/HeaderItemUiMapper.kt
@@ -8,14 +8,19 @@ import javax.inject.Inject
 
 class HeaderItemUiMapper @Inject constructor() {
 
+    private val locate = Locale.getDefault()
+    val dayFormat = SimpleDateFormat("dd", locate)
+    val dayOfTheWeekFormat = SimpleDateFormat("EEE", locate)
+    val monthYearFormat = SimpleDateFormat("MMMM yyyy", locate)
+
     fun toHeaderItemUiMapper(date: Date, sumOfTransactionsAmount: Float): HeaderItemUiModel {
         val locate = Locale.getDefault()
 
         return HeaderItemUiModel(
-            SimpleDateFormat("dd", locate).format(date),
-            SimpleDateFormat("EEE", locate).format(date).uppercase(),
-            SimpleDateFormat("MMMM yyyy", locate).format(date).uppercase(),
-            NumberFormat.getCurrencyInstance(locate).format(sumOfTransactionsAmount)
+            day = dayFormat.format(date),
+            dayOfTheWeek = dayOfTheWeekFormat.format(date).uppercase(),
+            monthYear = monthYearFormat.format(date).uppercase(),
+            sumOfTransactions = NumberFormat.getCurrencyInstance(locate).format(sumOfTransactionsAmount)
         )
     }
 }
